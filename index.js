bdbdba46c71a8182bfe1a9b273f4d338e47a8b3e
Rémi Treilhaud <77292380+rtreilhaud@users.noreply.github.com>
2022-01-03 11:42:59 +0100

    Add pug templates for main page

--- index.js
+++ index.js
@@ -2,11 +2,21 @@ const express = require('express');
 const app = express();
 const port = 9000;
 
-app.get('/', (req, res) => {
-	res.send('Hello World!');
-});
-
 app.use(express.static('public'));
+
+app.set('view engine', 'pug');
+
+app.get('/', function (req, res) {
+	const msg = [
+		{ user: 'pseudo1', text: 'Message example' },
+		{ user: 'pseudo2', text: 'Response example' },
+		{ user: 'pseudo3', text: '...' }
+	];
+	res.render('index', {
+		title: 'Chat room',
+		messages: msg
+	});
+});
 
 app.listen(port, () => {
 	console.log(`Example app listening at http://localhost:${port}`);
